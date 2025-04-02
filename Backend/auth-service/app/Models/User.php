8e66e77557307ecc2e29eb7f25a90724cc8ed5da
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-04-02 22:19:58 +0000

    Implement JWT authentication in User model by adding JWTSubject interface and custom claims

--- Backend/auth-service/app/Models/User.php
+++ Backend/auth-service/app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -17,12 +18,11 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+ 
 
+    protected $fillable = [
+        'name', 'email', 'password', 'role', 'phone', 'address', 'status'
+    ];
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -43,6 +43,18 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+        ];
+    }
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'role' => $this->role,
+            'email' => $this->email
         ];
     }
 }
