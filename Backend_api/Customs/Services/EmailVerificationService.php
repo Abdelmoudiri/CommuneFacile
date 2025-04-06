b47451d0500258c60a910f7e12d0e298ed1dcf60
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-04-06 20:59:58 +0100

    feat: implement EmailVerificationService for sending verification emails with token generation

---
+++ Backend_api/Customs/Services/EmailVerificationService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Customs\Services;
+
+use App\Models\EmailVerificationToken;
+use App\Notifications\EmailVerificationNotification;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Carbon;
+use Str;
+
+class EmailVerificationService
+{
+    public function sendVerificationEmail(Object $user)
+    {
+        Notification::send($user,new EmailVerificationNotification($this->generateVerificationLink($user->email)));
+
+
+    }
+    /**
+     * Génère un lien de vérification signé pour l’utilisateur.
+     */
+    public function generateVerificationLink(string $email): string
+    {
+        $checkIfTokenExists = EmailVerificationToken::where('email', $email)->first();
+        if ($checkIfTokenExists) {
+            $checkIfTokenExists->delete();
+        }
+        $token=Str::uuid();
+        $url=config('app.url')."?token=$token&email=$email";
+        $saveToken = EmailVerificationToken::create([
+            'email' => $email,
+            'token' => $token,
+            'expires_at' => Carbon::now()->addMinutes(60),
+        ]);
+        if ($saveToken) {
+            return $url;
+        }
+    }
+}
