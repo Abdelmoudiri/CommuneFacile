5998aab8bf79b9a09b82ef0824b8b4ce58b9bad5
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-04-06 19:29:42 +0100

    refactor: clean up API interceptors by removing redundant comments and whitespace

--- frontend/src/components/services/api.js
+++ frontend/src/components/services/api.js
@@ -6,11 +6,10 @@ const api = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
-    'Accept': 'application/json'
+    'Accept': 'application/json '
   }
 });
 
-// Request interceptor for adding the auth token
 api.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token');
@@ -24,7 +23,6 @@ api.interceptors.request.use(
   }
 );
 
-// Response interceptor for handling token expiration
 api.interceptors.response.use(
   response => {
     return response;
@@ -32,27 +30,19 @@ api.interceptors.response.use(
   error => {
     const originalRequest = error.config;
     
-    // If the error status is 401 and there is no originalRequest._retry flag,
-    // it means the token has expired and we need to refresh it
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       
-      // Try to refresh the token
       return api.post('/refresh')
         .then(res => {
           if (res.status === 200) {
-            // Save the new token
             localStorage.setItem('token', res.data.data.token);
-            
-            // Update the authorization header
             originalRequest.headers['Authorization'] = `Bearer ${res.data.data.token}`;
             
-            // Return the request with new token
             return api(originalRequest);
           }
         })
         .catch(refreshError => {
-          // If refresh token fails, log out the user
           localStorage.removeItem('token');
           localStorage.removeItem('user');
           window.location = '/login';
