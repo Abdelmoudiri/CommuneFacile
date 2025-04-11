cfcfd80bbaa84e34fcd676457a00ac6d553556ae
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-04-11 10:26:08 +0100

    feat: update User model to include role_id and is_active attributes; enhance UserController index method to return user data with roles and profiles

--- Backend_api/app/Http/Controllers/Api/V1/UserController.php
+++ Backend_api/app/Http/Controllers/Api/V1/UserController.php
@@ -10,8 +10,16 @@ class UserController extends Controller
 {
 
     public function index()
-    {return "true";
+    {
+        $query=User::with(['role','profile']);
+        dd($query);
+
+        return response()->json([
+            'status'=>'success',
+            'data'=>$query
+        ]);
     }
+
 
 
 
--- Backend_api/database/migrations/2025_04_11_090938_create_roles_table.php
+++ Backend_api/database/migrations/2025_04_11_090938_create_roles_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->json('permissions')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
