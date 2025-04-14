0152a49283e7d2fd8651f4d711014e7d189bf5f6
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-04-14 14:09:04 +0100

    ajouter les route du aythentification

--- LAcomuneAPI/routes/api.php
+++ LAcomuneAPI/routes/api.php
@@ -5,10 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
-Route::middleware(['auth:'])->group(function () {
-Route::post('logout',[AuthController::class,'logout']);
-Route::post('refresh',[AuthController::class,'refresh']);
-Route::post('forgot-password',[AuthController::class,'forgot']);
-Route::post('reset-password',[AuthController::class,'reste']);
-Route::post('verify-email',[AuthController::class,'verifyEmail']);
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('forgot-password', [AuthController::class, 'forgot']);
+    Route::post('reset-password', [AuthController::class, 'reset']);
+    Route::post('verify-email', [AuthController::class, 'verifyEmail']);
 });
