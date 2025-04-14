c711589ae573d890ad144bf6b111f2882216e96c
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-04-14 14:30:50 +0100

    creer le model Event avec migrations et seeder et controller

---
+++ LAcomuneAPI/database/migrations/2025_04_14_132037_create_evenments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evenments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('date');
+            $table->string('location')->nullable();
+            $table->foreignId('category_id')->constrained();
+            $table->boolean('is_published')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evenments');
+    }
+};
