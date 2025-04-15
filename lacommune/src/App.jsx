286ec1cb045ecb4cd7d00b1836ff950791784fa0
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-04-15 09:12:49 +0100

    ajouter events page au router

--- lacommune/src/App.jsx
+++ lacommune/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import Evenments from './pages/EventsPage'
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Autres routes existantes */}
+        <Route path="evenements" element={<Evenments/>}/>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
