d952dd85640f4cc1fbb1d780874d89e35af18336
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-05-16 18:15:50 +0100

    fix: correct relationship method in User model for role association

--- Backend_api/app/Models/User.php
+++ Backend_api/app/Models/User.php
@@ -28,7 +28,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function role()
     {
-        return $this->hasOne(Role::class,'role_id');
+        return $this->hasOne(Role::class,'id');
     }
     public function profile()
     {
